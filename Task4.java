25abb7930d643f10694d2351b037c97827f7fe2c
Satana500 <113170386+Satana500@users.noreply.github.com>
2022-11-07 16:19:31 +0200

    Update Task4.java

--- Task4.java
+++ Task4.java
@@ -5,12 +5,21 @@ public class Task4 {
             if(i%2!=0)a++;
         }
         int[] Mas = new int[a];
+        int count = 0;
+        int result = 0;
         for(int i=1,b=0;i<=99;i++){
-                if(i%2!=0){
-                    Mas[b]=i;
-                    System.out.print(Mas[b]+" ");
-                    b++;
-                }
+            if(i%2!=0){
+                Mas[b]=i;
+                result = result + Mas[b];
+                System.out.print(Mas[b]+" ");
+                b++;
+                count = count + 1;
+            } if(count==10){
+                System.out.printf("%n");
+                count=0;
+            }
+        }
+        result = result / Mas.length;
+        System.out.printf("Среднее арефметическое массива: %s%n", result);
         }
     }
-}
